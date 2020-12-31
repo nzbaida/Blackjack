7a94550b0bfde90661021a61782619e8f796fcc7
nzbaida <76817348+nzbaida@users.noreply.github.com>
2020-12-31 17:09:46 -0500

    Update blackjack.R
    deal and shuffle functions

--- blackjack.R
+++ blackjack.R
@@ -2,3 +2,12 @@ rm(list = ls())
 deck <- read.csv("deck.csv")
 deck$value[deck$face == "king" | deck$face == "queen" | deck$face == "jack"] <- 10
 deck$value[deck$face == "ace"] <- 11
+deal <- function(){
+  assign("deck", deck[-1,], parent.env(environment()))
+  card <- deck[1,]
+}
+shuffle <- function(deck){
+  Deck <- read.csv("deck.csv")
+  random <- sample(1:52, size = 52)
+  assign("deck", Deck[random,], parent.env(environment()))
+}
